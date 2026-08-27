9d8d72d30fae096720c81c8d2ce25c4cd2f0c0d4
Cleber-0101 <cleberbatista99@hotmail.com>
2026-08-27 13:37:17 -0300

    Ajustando end points

--- src/app.ts
+++ src/app.ts
@@ -5,6 +5,8 @@ import express from 'express';
 //iniciando 
 const app = express()
 
+
+//Criando end points
 //Configuração para receber um json e tranforma em Objeto 
 //para eu pode manipular esses dados
 app.use(express.json())
@@ -27,20 +29,23 @@ app.post('/partners', (req, res) => {
     const { name, email, password, company_name } = req.body
 })
 
-//Consumidor 
+
+//Consumidor-clientes
 app.post('/customers', (req, res) => {
     const { name, email, password, address, telefone } = req.body
 })
 
 //Criando evento
-app.post('/events', (req, res) => {
+app.post('/partners/events', (req, res) => {
     const { name, description, date, location } = req.body
 })
 
-// Buscando Evento
-app.get('/events', (req, res) => {
-    const {name , description, date} = req.body
+// buscando dados dos eventos 
+app.get('/partners/events', (req, res) => {
+
 })
+
+
 
 //Buscando evento por ID
 app.get('/events/:eventId', (req, res) => {
@@ -48,6 +53,23 @@ app.get('/events/:eventId', (req, res) => {
     console.log(eventId)
     res.send();
 })
+
+
+
+// listagem de eventos 
+// Buscando Evento
+app.get('/partners/events', (req, res) => {
+    const {name , description, date} = req.body
+})
+
+//Buscando evento por ID
+app.get('/parteners/events/:eventId', (req, res) => {
+    const {eventId} = req.params
+    console.log(eventId)
+    res.send();
+})
+
+
 
 app.listen(3000, () => {
     console.log('running in http://localhost:3000')
